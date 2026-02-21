61aba309ff421e7edb38bfcba73527dedc84d30a
yunagp04 <yunagp04@gmail.com>
2026-02-21 21:50:10 +0700

    update getUserIdFromProvider

--- backend/src/utils/getUserIdFromProvider.js
+++ backend/src/utils/getUserIdFromProvider.js
@@ -10,11 +10,22 @@ async function getUserIdFromProvider(pool, providerUserId) {
       WHERE provider_user_id = @provider_user_id
     `);
 
-  if (result.recordset.length === 0) {
-    throw new Error("User not found");
+  if (result.recordset.length > 0) {
+      return result.recordset[0].user_id;
   }
 
-  return result.recordset[0].user_id;
+  const newUserId = crypto.randomUUID();
+
+  await pool.request()
+    .input("user_id", sql.UniqueIdentifier, newUserId)
+    .input("providerId", sql.NVarChar, providerId)
+    .query(`
+      INSERT INTO UserAuth (user_id, provider_user_id, created_at)
+      VALUES (@user_id, @providerId, GETUTCDATE())
+    `);
+
+  return newUserId;
+  
 }
 
 export default getUserIdFromProvider;
